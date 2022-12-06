6b59974cf22190ad3163f07fb6f98a0c0de7ce5f
Zibing Zhang <zizhang@hubspot.com>
2022-12-06 12:26:29 +0000

    fix test

--- contacts/utils/file_io_utils_test.py
+++ contacts/utils/file_io_utils_test.py
@@ -9,12 +9,8 @@ from utils.file_io_utils import (
 
 
 def test_reading_and_writing_dataclasses_are_inverses():
-    object_1 = model.PhoneNumber(
-        country_code=model.CountryCode.NANP.value, number="1234"
-    )
-    object_2 = model.PhoneNumber(
-        country_code=model.CountryCode.CHINA.value, number="5678"
-    )
+    object_1 = model.PhoneNumber(country_code=model.CountryCode.NANP, number="1234")
+    object_2 = model.PhoneNumber(country_code=model.CountryCode.CHINA, number="5678")
     written_objects = [object_1, object_2]
     file_path = uuid_utils.generate()
 
